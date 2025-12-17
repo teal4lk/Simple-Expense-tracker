9eba103987230bf9c72388d730e3a53cf2f28edd
Leora Kelsey <teal4lk@gmail.com>
2025-12-16 21:22:41 -0600

    uploaded code and data to use with Java

---
+++ SimpleTracker.java
@@ -0,0 +1,182 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SimpleTracker {
+    private static final String CSV = "data_j.csv";
+    private static  BufferedReader reader = null;
+
+    /**
+     * Initializes the BufferedReader for reading the CSV file.
+     * @return BufferedReader object
+     */
+    private static BufferedReader initializeReader() {
+        try {
+            reader = new BufferedReader(new FileReader(CSV));
+            return reader;
+        } catch (IOException e) {
+            return null;
+        }
+    }
+    /**
+     * Checks if the CSV file exists.
+     * @return whether the file exists
+     */
+    private static boolean ifFileExist(){
+        try (BufferedReader br = new BufferedReader(new FileReader(CSV))) {
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+    /**
+     * Creates a new expense entry in the CSV file.
+     * @param date - the date of the expense
+     * @param category - the category of the expense
+     * @param amount - the amount of the expense
+     */
+    public void addExpense(Date date, String category, double amount) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        System.out.println(sdf.format(date));
+        if (!ifFileExist()) {
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(CSV))) {
+                writer.write("Date,Category,Amount\n");
+                writer.write(String.format("%s,%s,%.2f\n", sdf.format(date), category, amount));
+            } catch (IOException e) {
+                System.out.println("An error occurred while creating the CSV file.");
+                e.printStackTrace();
+            }
+        } else {
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(CSV, true))) {
+                        writer.write(String.format("%s,%s,%.2f\n", sdf.format(date), category, amount));
+                    } catch (IOException e) {
+                        System.out.println("An error occurred while adding the expense.");
+                        e.printStackTrace();
+                    }
+        }
+        
+    }
+    /**
+     * Calculates and prints the total expense from the CSV file.
+     */
+    public void getTotalExpense() {
+        double total = 0.0;
+        BufferedReader reader = initializeReader();
+        if (reader != null) {
+            String line;
+            try {
+                while ((line = reader.readLine()) != null) {
+                    String[] parts = line.split(",");
+                    
+                    if (!parts[2].equals("Amount")){
+                        total += Double.parseDouble(parts[2]);
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        System.out.printf("Total Expense: %.2f\n", total);
+    } else {
+        System.out.println("No expenses recorded yet.");
+    }
+    }
+    /**
+     * Calculates and prints the total expense by category from the CSV file.
+     */
+    public void getExpenseByCategory() {
+        Map<String, Double> categoryTotals = new HashMap<>();
+        BufferedReader reader = initializeReader(); 
+        if (reader != null) {
+            String line;
+            try { 
+                while ((line = reader.readLine()) != null) {
+                    String[] parts = line.split(",");
+                    String category = parts[1];
+                    if (!category.equals("Category")) {
+                        double amount = Double.parseDouble(parts[2]);
+                        categoryTotals.put(category, categoryTotals.getOrDefault(category, 0.0) + amount);
+                    } 
+                }
+            } catch (IOException e) {
+                e.printStackTrace(); 
+            }
+        } else {
+        System.out.println("No expenses recorded yet.");
+        return;
+        }
+        for (Map.Entry<String, Double> entry : categoryTotals.entrySet()) {
+            System.out.printf("Category: %s, Total Expense: %.2f\n", entry.getKey(), entry.getValue());
+        }
+    }
+    /**
+     * Calculates and prints the expense trend by date from the CSV file.
+     */
+    public void getExpenseTrend() {
+        Map<String, Double> dateTotals = new HashMap<>();
+        BufferedReader reader = initializeReader(); 
+        if (reader != null) {
+            String line;
+            try { 
+                while ((line = reader.readLine()) != null) {
+                    String[] parts = line.split(",");
+                    String date = parts[0];
+                    if (!date.equals("Date")) {
+                        double amount = Double.parseDouble(parts[2]);
+                        dateTotals.put(date, dateTotals.getOrDefault(date, 0.0) + amount);
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace(); 
+            }
+        } else {
+        System.out.println("No expenses recorded yet.");
+        return;
+        }   
+        String[] dates = dateTotals.keySet().toArray(new String[0]);
+        Arrays.sort(dates);
+        for (String date : dates) {
+            System.out.printf("Date: %s, Total Expense: %.2f\n", date, dateTotals.get(date));
+        }
+        
+    }
+    /**
+     * Calculates and prints the highest and lowest spend categories from the CSV file.
+     */
+    public void getHighestAndLowestSpendCategory() {
+        Map<String, Double> highest_lowest_map = new HashMap<>();
+        reader = initializeReader();
+        if (reader != null) {
+            String line;
+            try {
+                while ((line = reader.readLine()) != null) {
+                    String[] parts = line.split(",");
+                    String category = parts[1];
+                    if (!category.equals("Category")) {
+                        double amount = Double.parseDouble(parts[2]);
+                        highest_lowest_map.put(category, highest_lowest_map.getOrDefault(category, 0.0) + amount);
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            String highest_category = "";
+            String lowest_category = "";
+            for (Map.Entry<String, Double> entry : highest_lowest_map.entrySet()) {
+                if (entry.getValue() == Collections.max(highest_lowest_map.values())) {
+                    highest_category = entry.getKey();
+                }
+                if (entry.getValue() == Collections.min(highest_lowest_map.values())) {
+                    lowest_category = entry.getKey();
+                }
+            }
+            System.out.println("Highest Spend Category: " + highest_category + " \n Lowest Spend Category: " + lowest_category);
+        } else {
+            System.out.println("No expenses recorded yet.");
+        }
+    }
+}
---
+++ tracking_j.java
@@ -0,0 +1,110 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+public class tracking_j{
+    /**
+     * Main method to run the expense tracker program.
+     * @param args - command line arguments
+     */
+    public static void main(String[] args) {
+        boolean continue_program = true;
+        SimpleTracker tracking = new SimpleTracker();
+        String Options = "Options: \n"
+            + "1 - Adding an Expense to the tracker \n"
+            +"2 - Get Total Expense \n" 
+            +"3 - Get Expense by Category (optional -specify a category)\n" 
+            + "4 - Expense Trend \n" 
+            +"5 - Highest and Lowest Spend Category \n" 
+            + "Write exit or 0 to exit the program. \n";
+        System.out.println(Options);
+        while (continue_program) {
+            String user_input = System.console().readLine("What would you like to do? \n");
+            if (isNum(user_input)){
+                if (Integer.parseInt(user_input) == 1) {
+                    System.out.println("Adding an Expense to the tracker");
+                    String category = System.console().readLine("Enter category: ");
+                    String amount = System.console().readLine("Enter amount: ");
+                    while (!isNum(amount)){
+                        System.out.println("Invalid amount. Please enter a numeric value.");
+                        amount = System.console().readLine("Enter amount: ");
+                    }
+
+                    String date = System.console().readLine("Enter date (YYYY-MM-DD): ");
+                    boolean valid_date = false;
+                    Date date_expense = null;
+                    while(!valid_date && date.length() == 10) {
+                        if (isNum(date.substring(0,4))) {
+                            if (isNum(date.substring(5,7))) {
+                                if (isNum(date.substring(8,10))) {
+                                    valid_date = true;
+                                    try {
+                                        date_expense = new SimpleDateFormat("yyyy-MM-dd").parse(date);
+                                    } catch (Exception e) {
+                                        System.out.println("An error occurred while parsing the date.");
+                                        valid_date = false;
+                                    } 
+                                }
+                            }
+                        } else {
+                            System.out.println("Invalid date format. Please enter date in YYYY-MM-DD format.");
+                            date = System.console().readLine("Enter date (YYYY-MM-DD): ");
+                            valid_date = false;
+                        }
+                }
+                    // while (!valid_date) {
+                    //     try {
+                    //         date_expense = new SimpleDateFormat("yyyy-MM-dd").parse(date);
+                    //         valid_date = true;
+
+                    //     } catch (ParseException e) {
+                    //         System.out.println("Invalid date format. Please enter date in YYYY-MM-DD format.");
+                    //         date = System.console().readLine("Enter date (YYYY-MM-DD): ");
+                    //     }
+                    // }
+
+                    tracking.addExpense(date_expense, category, Double.parseDouble(amount));
+                
+                } else if (Integer.parseInt(user_input) == 2) {
+                    System.out.println("Getting Total Expense");
+                    tracking.getTotalExpense();
+                } else if (Integer.parseInt(user_input) == 3) {
+                    System.out.println("Getting Expense by Category");
+                    tracking.getExpenseByCategory();
+                } else if (Integer.parseInt(user_input) == 4) {
+                    System.out.println("Getting Expense Trend");
+                    tracking.getExpenseTrend();
+                } else if (Integer.parseInt(user_input) == 5) {
+                    System.out.println("Getting Highest and Lowest Spend Category");
+                    tracking.getHighestAndLowestSpendCategory();
+                } else if (Integer.parseInt(user_input) == 0 ) {
+                    continue_program = false;
+                    System.out.println("Thank you for using the Expense Tracker. Goodbye!");
+                } else {
+                    System.out.println("Invalid option. Please try again.");
+                }
+            } else {
+                if (user_input.equalsIgnoreCase("exit")) {
+                    continue_program = false;
+                    System.out.println("Thank you for using the Expense Tracker. Goodbye!");
+                } else { 
+                    System.out.println("Invalid option. Please try again.");
+                }
+            }
+        }
+    }
+    /**
+     * Checks if a string is numeric.
+     * @param str
+     * @return
+     */
+    public static boolean isNum(String str) {
+        for (char c : str.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
